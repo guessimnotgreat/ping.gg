d60c909c1a273d85edfaaadb32bb6e3da04ff2de
guessimnotgreat <148995092+guessimnotgreat@users.noreply.github.com>
2023-12-04 15:55:03 +0700

    Update serverless function

--- api/hello.js
+++ api/hello.js
@@ -1,4 +1,12 @@
-export default (req, res) => {
+// api/hello.js
+module.exports = async (req, res) => {
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+
+    // Your existing logic
     const { input1, input2 } = req.query;
     const result = `Input 1: ${input1}, Input 2: ${input2}`;
     res.status(200).send(result);
