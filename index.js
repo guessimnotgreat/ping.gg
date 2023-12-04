c414149a77345d5df2f342351172ae2d568519f7
guessimnotgreat <148995092+guessimnotgreat@users.noreply.github.com>
2023-12-04 15:38:52 +0700

    Update url to fetch

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+// index.js
 document.getElementById('myForm').addEventListener('submit', async function (e) {
     e.preventDefault();
 
     const input1 = document.getElementById('input1').value;
     const input2 = document.getElementById('input2').value;
 
-    const response = await fetch(`http://localhost:3000/hello?input1=${encodeURIComponent(input1)}&input2=${encodeURIComponent(input2)}`);
+    const response = await fetch(`http://ping-gg-guessimnotgreats-projects.vercel.applocalhost:3000/hello?input1=${encodeURIComponent(input1)}&input2=${encodeURIComponent(input2)}`);
     const result = await response.text();
     document.getElementById('result').innerText = result;
 });
